877aa38c2037704b4c0a4766cd3d7cd0ec8da540
Joseph Kahn <josephbkahn@gmail.com>
2014-03-31 00:04:49 -0400

    linting the replay file

--- public/js/replay.js
+++ public/js/replay.js
@@ -1,152 +1,162 @@
-var Replay = function() {
-  function indexOfEqual(arr, el) {
-    return _.indexOf(arr, _.find(arr, _.partial(_.isEqual, el)));
-  }
-  var situation = {};
-  this.situation = situation;
-
-  function findPlayer(id) {
-    return _.find(situation.players, function(player) { return player.id === id; });
-  }
-
-  this.initial_situation = [
-    function(e) {
-      _.extend(situation, clone(e.situation));
+/*global _, clone*/
+var Replay = function () {
+    "use strict";
+    function indexOfEqual(arr, el) {
+        return _.indexOf(arr, _.find(arr, _.partial(_.isEqual, el)));
     }
-  ];
+    var situation = {};
+    this.situation = situation;
 
-  this.draw_and_discard_infection_card = [
-    function(e) {
-      var index = indexOfEqual(situation.infection_cards_draw, e.card);
-      var removed = situation.infection_cards_draw.splice(index, 1);
-      situation.infection_cards_discard.unshift(removed[0]);
+    function findPlayer(id) {
+        return _.find(situation.players, function (player) { return player.id === id; });
     }
-  ];
 
-  this.infect = [
-    function(e) {
-      var location = _.find(situation.locations,
-        function(location) { return location.name === e.location; });
-      location.infections[e.disease]++;
-      var disease = _.find(situation.diseases,
-        function(disease) { return disease.name === e.disease; });
-      disease.cubes--;
-    }
-  ];
+    this.initial_situation = [
+        function (e) {
+            _.extend(situation, clone(e.situation));
+        }
+    ];
 
-  this.treat_disease = [
-    function(e) {
-      var location = _.find(situation.locations,
-        function(location) { return location.name === e.location; });
-      location.infections[e.disease] -= e.number;
-      var disease = _.find(situation.diseases,
-        function(disease) { return disease.name === e.disease; });
-      disease.cubes += e.number;
-    }
-  ];
+    this.draw_and_discard_infection_card = [
+        function (e) {
+            var index = indexOfEqual(situation.infection_cards_draw, e.card),
+                removed = situation.infection_cards_draw.splice(index, 1);
+            situation.infection_cards_discard.unshift(removed[0]);
+        }
+    ];
 
-  this.draw_player_card = [
-    function(e) {
-      var index = indexOfEqual(situation.player_cards_draw, e.card);
-      var removed = situation.player_cards_draw.splice(index, 1);
-      var player = findPlayer(e.player);
-      if (e.card.type !== "epidemic") player.hand.push(removed[0]);
-    }
-  ];
+    this.infect = [
+        function (e) {
+            var location, disease;
+            location = _.find(situation.locations,
+                function (location) { return location.name === e.location; });
+            location.infections[e.disease] = location.infections[e.disease] + 1;
+            disease = _.find(situation.diseases,
+                function (disease) { return disease.name === e.disease; });
+            disease.cubes = disease.cubes - 1;
+        }
+    ];
 
-  this.discard_player_card = [
-    function(e) {
-      var player = findPlayer(e.player);
-      var index = indexOfEqual(player.hand, e.card);
-      var removed = player.hand.splice(index, 1);
-      situation.player_cards_discard.unshift(removed[0]);
-    }
-  ];
+    this.treat_disease = [
+        function (e) {
+            var location, disease;
+            location = _.find(situation.locations,
+                function (location) { return location.name === e.location; });
+            location.infections[e.disease] -= e.number;
+            disease = _.find(situation.diseases,
+                function (disease) { return disease.name === e.disease; });
+            disease.cubes += e.number;
+        }
+    ];
 
-  this.build_research_center = [
-    function(e) {
-      situation.research_centers.push({ "location": e.location });
-      situation.research_centers_available--;
-    }
-  ];
+    this.draw_player_card = [
+        function (e) {
+            var index = indexOfEqual(situation.player_cards_draw, e.card),
+                removed = situation.player_cards_draw.splice(index, 1),
+                player = findPlayer(e.player);
+            if (e.card.type !== "epidemic") {
+                player.hand.push(removed[0]);
+            }
+        }
+    ];
 
-  this.discard_discarded_city = [
-    function(e) {
-      situation.infection_cards_discard = _.filter(situation.infection_cards_discard, function(card) { return card.location !== e.location });
-    }
-  ];
+    this.discard_player_card = [
+        function (e) {
+            var player = findPlayer(e.player),
+                index = indexOfEqual(player.hand, e.card),
+                removed = player.hand.splice(index, 1);
+            situation.player_cards_discard.unshift(removed[0]);
+        }
+    ];
 
-  this.state_change = [
-    function(e) {
-      situation.state = e.state;
-    }
-  ];
+    this.build_research_center = [
+        function (e) {
+            situation.research_centers.push({ "location": e.location });
+            situation.research_centers_available = situation.research_centers_available - 1;
+        }
+    ];
 
-  this.infection_rate_increased = [
-    function(e) {
-      situation.infection_rate_index++;
-    }
-  ];
+    this.discard_discarded_city = [
+        function (e) {
+            situation.infection_cards_discard = _.filter(situation.infection_cards_discard, function (card) {
+                return card.location !== e.location;
+            });
+        }
+    ];
 
-  this.infection_cards_restack = [
-    function(e) {
-      situation.infection_cards_discard = [];
-      situation.infection_cards_draw = clone(e.cards).concat(situation.infection_cards_draw);
-    }
-  ];
+    this.state_change = [
+        function (e) {
+            situation.state = e.state;
+        }
+    ];
 
-  this.outbreak = [
-    function(e) {
-      situation.outbreak_count++;
-    }
-  ];
+    this.infection_rate_increased = [
+        function (e) {
+            situation.infection_rate_index = situation.infection_rate_index + 1;
+        }
+    ];
 
-  this.move_pawn = [
-    function(e) {
-      var player = findPlayer(e.player);
-      player.location = e.location;
-    }
-  ];
+    this.infection_cards_restack = [
+        function (e) {
+            situation.infection_cards_discard = [];
+            situation.infection_cards_draw = clone(e.cards).concat(situation.infection_cards_draw);
+        }
+    ];
 
-  this.discover_cure = [
-    function(e) {
-      var disease = _.find(situation.diseases,
-          function(disease) { return disease.name === e.disease; });
-      disease.status = "cure_discovered";
-    }
-  ];
+    this.outbreak = [
+        function (e) {
+            situation.outbreak_count = situation.outbreak_count + 1;
+        }
+    ];
 
-  this.eradicate_disease = [
-    function(e) {
-      var disease = _.find(situation.diseases,
-          function(disease) { return disease.name === e.disease; });
-      disease.status = "eradicated";
-    }
-  ];
+    this.move_pawn = [
+        function (e) {
+            var player = findPlayer(e.player);
+            player.location = e.location;
+        }
+    ];
 
-  this.transfer_player_card = [
-    function(e) {
-      var from = findPlayer(e.from_player);
-      var to = findPlayer(e.to_player);
-      var index = indexOfEqual(from.hand, e.card);
-      var card = from.hand.splice(index, 1);
-      to.hand.push(card[0]);
-    }
-  ];
+    this.discover_cure = [
+        function (e) {
+            var disease = _.find(situation.diseases,
+                    function (disease) { return disease.name === e.disease; });
+            disease.status = "cure_discovered";
+        }
+    ];
 
-  this.approve_action = [
-    function(e) {
-      situation.state = situation.state.parent;
-    }
-  ]
+    this.eradicate_disease = [
+        function (e) {
+            var disease = _.find(situation.diseases,
+                    function (disease) { return disease.name === e.disease; });
+            disease.status = "eradicated";
+        }
+    ];
 
-  this.one_quiet_night = [
-    function(e) {
-      situation.quiet_night = true;
-    }
-  ]
+    this.transfer_player_card = [
+        function (e) {
+            var from = findPlayer(e.from_player),
+                to = findPlayer(e.to_player),
+                index = indexOfEqual(from.hand, e.card),
+                card = from.hand.splice(index, 1);
+            to.hand.push(card[0]);
+        }
+    ];
 
-  this.receive = function(e) {
-    _.each(this[e.event_type], function(f) { f(e) });
-  }
+    this.approve_action = [
+        function (e) {
+            situation.state = situation.state.parent;
+        }
+    ];
+
+    this.one_quiet_night = [
+        function (e) {
+            situation.quiet_night = true;
+        }
+    ];
+
+    this.receive = function (e) {
+        _.each(this[e.event_type], function (f) {
+            f(e);
+        });
+    };
 };
